0a23c90dce4c711f35c6e69d2b5440ef1c1fd2eb
repeater1 <2573205534@qq.com>
2024-05-13 14:12:01 +0800

    message20: testing for layout + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -104,3 +104,27 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertIn('Buy milk', page_text)
 
         #两人都很满意，然后去睡觉了
+
+    def test_layout_and_styling(self):
+        #访问首页
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024,768)
+
+        #输入框居中显示     
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+            )
+        
+        #新建清单，输入框居中显示     
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)   
+        self.wait_for_row_in_list_table('1: testing')
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+            )
