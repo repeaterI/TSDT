b2f551ae9834688d6cd7b777f4ce7632007af093
repeater1 <2573205534@qq.com>
2024-05-30 14:12:52 +0800

    message26:add gunicorn to virtualenv requirements

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -7,12 +7,16 @@ import time
 from selenium.webdriver.common.by import By
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium.common.exceptions import WebDriverException
+import os
 
 MAX_WAIT = 10 #(1)
 
 class NewVisitorTest(StaticLiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
+        real_server = '8.130.102.214' #(1)
+        # if real_server:
+        self.live_server_url = 'http://' + real_server #(2)
 
     def tearDown(self):
         self.browser.quit()
--- notes/settings.py
+++ notes/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^vql)w(pon&p#n3wtc7)7tw)q!jh6=#!=v6uw4+mme@srr@&es'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+TEMPLATES_DEBUG =DEBUG
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['8.130.102.214']
 
 
 # Application definition
